33938f5ad9e1b0f3e4b610f34ef297738e908188
Beau <beau.mahadev@energybox.com>
2019-02-12 18:22:49 +0800

    calculate thermal comfort using clothing index from GPS coordinates

---
+++ testThermalComfort.py
@@ -0,0 +1,8 @@
+import thermalComfort
+
+coordinateX= 22.428005
+coordinateY= 114.210902
+indoorTemp= 27
+indoorHumidity= 60
+
+print(thermalComfort.getThermalComfort(indoorTemp,indoorHumidity,coordinateX,coordinateY))
