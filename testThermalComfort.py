f53c9ce768e0b5b6670faf7cd1846b77d834fa04
Beau <beau.mahadev@energybox.com>
2019-02-13 11:54:14 +0800

    deleting old files

--- testThermalComfort.py
+++
@@ -1,8 +0,0 @@
-import thermalComfort
-
-coordinateX= 22.428005
-coordinateY= 114.210902
-indoorTemp= 27
-indoorHumidity= 60
-
-print(thermalComfort.getThermalComfort(indoorTemp,indoorHumidity,coordinateX,coordinateY))
