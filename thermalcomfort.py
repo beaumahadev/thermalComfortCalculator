f280557b67c7269741e9fd33f344466941527fb0
Beau <beau.mahadev@energybox.com>
2019-02-13 13:31:33 +0800

    finalizing real feel calculation and cloth index estimation

--- thermalcomfort.py
+++ thermalcomfort.py
@@ -1,34 +1,55 @@
 import pyowm
+import math
 import fangers
 
-def getThermalComfort(indoorTemp, indoorHumidity, coordinateX, coordinateY):
+def getThermalComfort(indoor_temp, indoor_humidity, coordinateX, coordinateY):
+
     #Personal OpenWeatherMap API Key
     owm = pyowm.OWM('7c759896dc1e8fdb39b532eeaa14641e')
-    # Get weather at coordinates- temp, humidity
+
+    # Get weather at coordinates- temp, humidity, windspeed
     observation = owm.weather_at_coords(coordinateX, coordinateY)
     w = observation.get_weather()
     outdoorTemp=w.get_temperature()['temp']-273.15
     outdoorHumidity=w.get_humidity()
+    outdoorWindSpeed=w.get_wind()['speed']
 
-    #To Do: Calculate Real Feel
-    realTemp=outdoorTemp
+    #Calculate the felt temperature
+    real_temp=calculate_realfeel(outdoorTemp,outdoorHumidity, outdoorWindSpeed)
 
-    #Standard indoor clothing for hot weather is .5
-    #Standard indoor clothing for cold weather is 1.0
-    #There is a linear relationship (refer to ASHRAE standards)
-
-    #Lets say clothing index is 1 at 5C and .5 at 26C. Then...
-
-    clothing_index= (realTemp*-.0238) + 1.12
-    
+    #Get clothing index
+    clothing_index= calculate_clothindex(real_temp) 
 
     #Estimate metobolic rate
     #Seated: 1.1
     #Cooking: 1.8
     metobolic_rate=1.1
 
-    #Calculate thermal comfort 
+    #Calculate thermal comfort using Fanger's method
+    return fangers.calculate(indoor_temp, indoor_humidity, metobolic_rate,clothing_index)
 
-    return fangers.calculate(indoorTemp, indoorHumidity, metobolic_rate,clothing_index)
+
+def calculate_realfeel(temp,humidity,wind):
+    #tested against Australian Bureau of Meteorology formula
+    e= humidity/100 * 6.105 * math.exp((17.27*temp)/(237.7+temp))
+    real_temp= temp + 0.348 * e - 0.7 * wind + .7/(wind+10) - 4.25
+    return real_temp
+
+
+
+
+def calculate_clothindex(temp):
+    #Standard indoor clothing for hot weather is .5
+    #Standard indoor clothing for cold weather is 1.0
+    #There is a linear relationship (refer to ASHRAE standards)
+
+    #Lets say clothing index is 1 at 10C and .5 at 26C. Then...
+    index= (temp*-.03125) + 1.3125
+    if index > 1:
+        return 1
+    elif index < .5:
+        return .5
+    else:
+        return index
 
 
